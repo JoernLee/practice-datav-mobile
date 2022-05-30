fa328d7b47677c75e401d986779f997247d3245a
Joern <924901972@qq.com>
2022-05-30 21:39:47 +0800

    chore:init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import VueRouter from 'vue-router'
+import './styles/reset.css'
+import ECharts from 'echarts';
+import VueECharts from 'vue-echarts';
+
+Vue.use(VueRouter)
 
 Vue.config.productionTip = false
 
+Vue.prototype.$echarts = ECharts;
+
+Vue.component('vue-echarts', VueECharts);
+
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="home">
+    <div class="echarts-wrapper">
+      <vue-echarts :options="options"/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  data() {
+    return {
+      options: {}
+    }
+  },
+  mounted() {
+    this.options = {
+      title: {
+        text: '入门示例'
+      },
+      tooltip: {},
+      xAxis: {
+        data: ['衣服', '裤子']
+      },
+      yAxis: {},
+      series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20]
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.home {
+  font-size: 32px;
+}
+
+.echarts-wrapper {
+  width: 100%;
+  height: 500px;
+}
+
+.echarts {
+  width: 100%;
+  height: 100%;
+}
+</style>
